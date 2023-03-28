02564c96193b02a76295ea4d295f047a3c09f62f
Aleksandr Karavaev <sandro-wold@live.ru>
2023-03-29 00:15:57 +0500

    Домашнее задание №13 по уроку "Объекты и классы" корректировка №1

--- src/homework13/Book.java
+++ src/homework13/Book.java
@@ -2,15 +2,21 @@ package homework13;
 
 public class Book {
     private String bookName;
+    private String author;
     private int publicationYear;
 
-    public Book(String bookName, int publicationYear) {
+    public Book(String bookName, String author, int publicationYear) {
         this.bookName = bookName;
+        this.author = author;
         this.publicationYear = publicationYear;
     }
 
     public String getBookName() {
         return this.bookName;
+    }
+
+    public String getAuthor() {
+        return this.author;
     }
 
     public int getPublicationYear() {
--- src/homework13/Main.java
+++ src/homework13/Main.java
@@ -4,12 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         Author dostoevsky = new Author("Федор", "Достоевский");
-        Book besy = new Book("Бесы", 1872);
+        Book besy = new Book("Бесы", "Ф.М. Достоевский", 1872);
+        System.out.println("Автор книги - " + besy.getAuthor());
         System.out.println("Автор книги - " + dostoevsky.getFirstName() + " " + dostoevsky.getLastName());
         System.out.println("Книга - " + besy.getBookName() + "," + " год публикации " + besy.getPublicationYear());
         Author pushkin = new Author("Александр", "Пушкин");
-        Book onegin = new Book("Евгений Онегин", 1833);
         System.out.println("Автор книги - " + pushkin.getFirstName() + " " + pushkin.getLastName());
+        Book onegin = new Book("Евгений Онегин", "А.С. Пушкин", 1833);
+        System.out.println("Автор книги - " + onegin.getAuthor());
         System.out.println("Книга - " + onegin.getBookName() + "," + " год публикации " + onegin.getPublicationYear());
         System.out.println();
         besy.setPublicationYear(1871);
